507198d40f354d9d55ae038e11225551095d8482
DIR\renata.jegereva <renata@webcreating.co.uk>
2021-05-17 22:32:05 +0200

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './Home';
+import Nav from './Nav';
+import Profile from './Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className="body">
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </>
   );
 }
 
